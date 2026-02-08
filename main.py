27256ef34734680aa6034f1a3090eb9484164ebc
tgnd254 <estefania.ddelgado@estudiantes.matcom.uh.cu>
2026-02-08 01:43:00 -0500

    Agrego de imagenes

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from kivy.config import Config 
+# Bloquear redimensionamiento 
+Config.set('graphics', 'resizable', False) 
+# Definir tamaño fijo de la ventana 
+Config.set('graphics', 'width', '800') 
+Config.set('graphics', 'height', '600')
+
+# Desactivar el puntico rojo de clic derecho
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
@@ -18,8 +28,19 @@ class HomeScreen(Screen):
         root=FloatLayout()
         background=Image(source="images/refugio.png", allow_stretch=True, keep_ratio=False)
         root.add_widget(background)
-        button_crear_evento = ImageButton(source="images/crear_evento.png", size_hint=(0.2, 0.3),pos_hint={"center_x":0.1, "center_y":0.8})
-        button_eventos_creados = ImageButton(source="images/eventos_creados.png", size_hint=(0.2, 0.3),pos_hint={"center_x":0.1, "center_y":0.6})
+
+        button_crear_evento = ImageButton(
+            source="images/crear_evento.png", 
+            size_hint=(0.3, 0.3),
+            pos_hint={"center_x":0.13, "center_y":0.85}
+        )
+
+        button_eventos_creados = ImageButton(
+            source="images/eventos_creados.png", 
+            size_hint=(0.3, 0.3),
+            pos_hint={"center_x":0.13, "center_y":0.6}
+        )
+
         button_eventos_creados.bind(on_press=self.go_to_events)
         button_crear_evento.bind(on_press=self.go_to_place) 
         root.add_widget(button_crear_evento)
